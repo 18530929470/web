b4112d03122784190ca15ede0a883ccd04714b7b
qinghua.wang <qinghua.wang@broadfun.cn>
2020-10-23 19:48:52 +0800

    add

--- test.py
+++ test.py
@@ -5,12 +5,41 @@
 # @File : test.py
 # @Software : PyCharm
 
-
+# coding=utf-8
+import unittest
 from selenium import webdriver
+import time
 
-driver = webdriver.Firefox()
-driver.get('http://www.baidu.com')
-driver.implicitly_wait(10)
 
-driver.find_element_by_name("wd").send_keys("周杰伦")
-driver.find_element_by_id("su").click()
+class forTest(unittest.TestCase):
+    # 测试用例初始化
+    # 打开谷歌浏览器，并进入百度
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome()
+
+        self.driver.get('http://www.baidu.com')
+
+    # 测试用例释放
+    # 等待 3s，关闭浏览器
+    def tearDown(self) -> None:
+        time.sleep(3)
+
+        self.driver.quit()
+
+    # 输入‘战狼'，并点击搜索
+    def test_1(self):
+        pass
+
+        self.driver.find_element_by_id('kw').send_keys('战狼')
+        self.driver.find_element_by_id('su').click()
+
+    # 输入‘红海行动'，并点击搜索
+    def test_2(self):
+        pass
+
+        self.driver.find_element_by_id('kw').send_keys('红海行动')
+        self.driver.find_element_by_id('su').click()
+
+
+if __name__ == '__main__':
+    unittest.main()
